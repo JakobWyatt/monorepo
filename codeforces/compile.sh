b10e463c0ae0a0c372a18b82ba0c1497818dd1ae
Jakob Wyatt <jakob.wyatt@gmail.com>
2025-04-05 10:07:40 +0000

    up

--- codeforces/compile.sh
+++ codeforces/compile.sh
@@ -1,2 +1,1 @@
-/home/vscode/llvm-project/build/bin/clang++ -std=c++23 -O2 -Wall -stdlib=libc++ 148_gray.cpp
-LD_PRELOAD=/home/vscode/llvm-project/build/lib/x86_64-unknown-linux-gnu/libc++.so.1:/home/vscode/llvm-project/build/lib/x86_64-unknown-linux-gnu/libc++abi.so.1:/home/vscode/llvm-project/build/lib/x86_64-unknown-linux-gnu/libunwind.so.1 ./a.out
+/home/vscode/llvm-project/build/bin/clang++ -stdlib=libc++ -lc++abi -pthread -fuse-ld=lld -static -Wall -Wformat -Wconversion -std=c++23 -O2 $1
